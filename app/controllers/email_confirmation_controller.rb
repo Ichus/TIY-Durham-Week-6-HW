a3652368c778479ac8599d0d76c59ea70b35f72b
Ichus <Ichus.local>
2014-07-13 18:43:31 -0400

    Attempt to fix account verification

--- app/controllers/email_confirmation_controller.rb
+++ app/controllers/email_confirmation_controller.rb
@@ -3,7 +3,5 @@ class EmailConfirmationController < ApplicationController
     confirm = EmailConfirmation.find_by_token(params[:id])
     @user = User.find(confirm.user_id)
     confirm.destroy
-    Sleep 5.0
-    redirect_to users_path(@user.id)
   end
 end
